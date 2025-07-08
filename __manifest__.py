83903c0423d579456b4bb80a800a0337d09f0695
Maizar Rahman <maizarrahman1@gmail.com>
2025-07-08 17:00:18 +0700

    hapus oca-gen

--- __manifest__.py
+++ __manifest__.py
@@ -1,34 +1,24 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Latihan Odoo 17",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
+    "name": "Latihan Odoo 17",
+    "summary": "Latihan Odoo 17",
+    "author": "Maizar, Odoo Community Association (OCA)",
+    "website": "https://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    "category": "Uncategorized",
+    "version": "17.0.0.0.1",
+    "license": "LGPL-3",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        "views/views.xml",
+        "views/templates.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
-
